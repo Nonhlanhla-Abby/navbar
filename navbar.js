18c301ffeabf160dea335b841c3b2fcfd043a0f9
Nonhlanhla-Abby <103173020+Nonhlanhla-Abby@users.noreply.github.com>
2022-04-26 14:52:08 +0200

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,7 @@
+echo "# navbar" >> add.
+git init
+git add.
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Nonhlanhla-Abby/navbar.git
+git push -u origin main
